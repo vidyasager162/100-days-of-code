cae374bde30829fa90343be7aa8c23ffb1a9b8f9
Vidyasager <vidyasager162@gmail.com>
2023-06-11 19:09:29 +0530

    Finish Day 6

---
+++ Day 6/reeborg_hurdle2.py
@@ -0,0 +1,19 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+    
+# number_of_hurdles = 6
+# while number_of_hurdles > 0:
+#     move()
+#     jump()
+#     number_of_hurdles -= 1
